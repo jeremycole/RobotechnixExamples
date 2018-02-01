f3511d6a7b9000c64fcdcaca6a217f63c96f8cbb
Jeremy Cole <jeremy@jcole.us>
2018-02-01 10:48:26 -0800

    Add a bunch of additional explanation in AutonomousElapsedTimeBoxPattern

--- src/main/java/com/robotechnix/examples/AutonomousElapsedTimeBoxPattern.java
+++ src/main/java/com/robotechnix/examples/AutonomousElapsedTimeBoxPattern.java
@@ -5,6 +5,9 @@ to be adjusted to match the speed and agility of a particular robot. This was
 tested with a direct-drive robot using NeveRest 40 motors and mecanum wheels
 and makes a nearly perfect box within about 10 feet of space.
 
+This OpMode uses empty while() loops to wait until the specified time has
+elapsed, and uses an ElapsedTime object to count the passing of time.
+
 Required hardware:
   1. Four motors should be connected to motor controllers and named for their
      position on the robot: "mFL", "mFR", "mBL", "mBR", where "F" means front,
@@ -30,13 +33,32 @@ public class AutonomousElapsedTimeBoxPattern extends LinearOpMode {
     // the overall size of the box.
     private static final double DRIVE_TIME = 2.0;
 
+    // The speeds that the robot should drive. Variables are used here so that
+    // they can be easily changed without needing to change it in multiple
+    // places below.
     private static final double STOP_POWER = 0.0;
     private static final double TURN_POWER = 0.3;
     private static final double DRIVE_POWER = 0.5;
 
+    // A variable for each motor. Since they are initialized and used in
+    // separate methods, these variables must be "instance variables" in the
+    // class (outside of any method) so that they can be accessed from any of
+    // the methods here.
+    //
+    // These are all *null* initially, and must be initialized before they can
+    // be used (otherwise the program will produce a NullPointerException and
+    // crash). See doInitialization()!
     private DcMotor mFL, mFR, mBL, mBR;
 
     private void doInitialization() {
+        // Initialize all motors. Since they are mounted opposed to each other,
+        // one side's motors (the left in this case) are "forward" and the
+        // other side (right) "reverse".
+        //
+        // The names of the motors can be adjusted here to match your robot.
+        //
+        // The directions can be adjusted until the robot is driving straight
+        // and turning right as designed.
         mFL = hardwareMap.dcMotor.get("mFL");
         mFL.setDirection(DcMotorSimple.Direction.FORWARD);
         mFR = hardwareMap.dcMotor.get("mFR");
@@ -66,8 +88,8 @@ public class AutonomousElapsedTimeBoxPattern extends LinearOpMode {
         mBR.setPower(-TURN_POWER);
 
         // Wait for TURN_TIME to elapse.
-        while (!isStopRequested() && elapsedTime.time() < TURN_TIME) {
-            // Do nothing.
+        while (!opModeIsActive() && elapsedTime.time() < TURN_TIME) {
+            // Do nothing else, just wait.
         }
 
         // Stop movement.
@@ -83,9 +105,9 @@ public class AutonomousElapsedTimeBoxPattern extends LinearOpMode {
         mFR.setPower(DRIVE_POWER);
         mBR.setPower(DRIVE_POWER);
 
-        // Wait for TURN_TIME to elapse.
-        while (!isStopRequested() && elapsedTime.time() < DRIVE_TIME) {
-            // Do nothing.
+        // Wait for DRIVE_TIME to elapse.
+        while (!opModeIsActive() && elapsedTime.time() < DRIVE_TIME) {
+            // Do nothing else, just wait.
         }
 
         // Stop movement.
@@ -94,7 +116,8 @@ public class AutonomousElapsedTimeBoxPattern extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Run our initialization code (in its own method to keep this one cleaner).
+        // Run our initialization code (in its own method to keep this method
+        // smaller and cleaner).
         doInitialization();
 
         // Wait for the driver to press the "Start" button.
@@ -127,6 +150,7 @@ public class AutonomousElapsedTimeBoxPattern extends LinearOpMode {
         // Make sure the robot is stopped.
         doStop();
 
-        // Exit here, ending the program.
+        // Exit here by "falling off" the end of runOpMode() method, which
+        // will end the program.
     }
 }
